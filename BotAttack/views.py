7a7e795680f1398727c3399d5dc0f104406b433f
Harsh <142230924+HarshModi2005@users.noreply.github.com>
2024-02-11 16:12:57 +0530

    Add files via upload

---
+++ BotAttack/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def botattack(request):
+    return render(request,'service-page.html')
---
+++ RansomWWare/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ransomware(request):
+    return render(request,'service-page.html')
